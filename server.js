5051a1652f8203ebd8113ecc3cccd1b2d2c645f8
Ebeso Joseph <codewithebs@gmail.com>
2023-07-30 18:49:47 +0100

    made authentication changes with respect to current specification

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const { OAuth2Client } = require("google-auth-library");
+const client = new OAuth2Client();
+require("dotenv").config();
+const CLIENT_ID = process.env.CLIENT_ID;
+
+async function verify(token) {
+  const ticket = await client.verifyIdToken({
+    idToken: token,
+    audience: CLIENT_ID,
+  });
+  return ticket;
+}
+
+app.use(morgan("combined"));
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Middleware
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-})
+app.get("/", (req, res) => {
+  const { data } = req.query;
+  console.log(data);
+  const result = JSON.parse(data);
+  res.render("../index",result);
+});
 
-app.get('/login', (req, res) => {
-    res.render('../login')
-})
+app.post("/login", async (req, res) => {
+  const { credential } = req.body;
+  const payload = await verify(credential);
+  if(!payload) res.redirect('/login');
+  console.log(payload.payload);
+  res.redirect(`/?data=${JSON.stringify(payload.payload)}`);
+});
+
+app.get("/login", (req, res) => {
+  console.log(req.body);
+  res.render("../login");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
